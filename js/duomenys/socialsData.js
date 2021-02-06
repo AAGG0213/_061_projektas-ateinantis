099fd2d0a92e3a64b8554b5d112aefcd607c0d2d
AAGG0213 <Aurelija.Gefeniene@gmail.com>
2021-02-06 12:12:00 +0200

    Socials, validacija ir funkcijos

---
+++ js/duomenys/socialsData.js
@@ -0,0 +1,38 @@
+const socialsData = [
+    {
+        href: '#',
+        icon: 'facebook'
+    },
+// 12341234,
+
+    {
+        href: '#',
+        icon: 'twitter'
+    },
+
+    {
+        href: '#',
+        icon: 'dribbble'
+    },
+    {
+        href: '#',
+        icon: 'tumblr'
+    },
+   
+    {
+        href: '#',
+        icon: 'linkedin'
+    },
+
+    {
+        href: '#',
+        icon: 'instagram'
+    },
+
+    {
+        href: '#',
+        icon: 'rss'
+    },
+]
+
+export { socialsData }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('Jau greit ateis');
+// iskvieti funkcija
+import { renderSocials } from './komponentai/renderSocials.js';
+// paimti duomenis
+import { socialsData } from './duomenys/socialsData.js';
+//funkcijai perduoti duomenis
+// renderSocials(socialsData);
+
+
+// universaliau butu daryti funkcija (selektorius, duomenys):
+// pvz., renderSocials('footer > .social', socialsData);
+// tada reiketu papildomu validaciju selektoriui
+
+renderSocials('footer > .social', socialsData);
+
